3c217bb6a154a2da0bbcbd555dd83db69107589f
Adolfo Reyes <adolfo0620@gmail.com>
2017-06-15 15:24:38 -0400

    refactoring queue

--- Stacks-And-Queues/Queue.py
+++ Stacks-And-Queues/Queue.py
@@ -5,22 +5,22 @@ class Queue(list):
 	Data structure definition 
 	'''
 	def __init__(self, queue=[]):
-		self.queue = queue
+		self.extend(queue)
 	
 	def enqueue(self, item):
 		'''
 		adds item to the queue as index -1
 		return queue
 		'''
-		self.queue.append(item)
-		return self.queue
+		self.append(item)
+		return self
 
 	def dequeue(self):
 		'''
 		remove item at 0 and return its
 		'''
-		if self.queue:
-			return self.queue.pop(0)
+		if self:
+			return self.pop(0)
 
 
 class TestQueue(unittest.TestCase):
@@ -31,15 +31,17 @@ class TestQueue(unittest.TestCase):
 	def test_dequeue(self):
 		queue = Queue(["a","b","c"])
 		self.assertEqual(queue.dequeue(), "a")
-		self.assertEqual(queue.queue, ["b","c"])
+		self.assertEqual(queue, ["b","c"])
 
-		queue = Queue([])
+		queue = Queue()
+		self.assertEqual(queue, [])
 		self.assertEqual(queue.dequeue(), None)
+
 
 	def test_init(self):
 		queue_list = ["a","b","c"]
 		queue = Queue(queue_list)
-		self.assertEqual(queue.queue, queue_list)
+		self.assertEqual(queue, queue_list)
 
 if __name__ == '__main__':
     unittest.main()
