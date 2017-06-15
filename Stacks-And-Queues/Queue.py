848aac6fa93c09e8ccdaef50b16a5bd3336de490
Adolfo Reyes <adolfo0620@gmail.com>
2017-06-15 14:35:08 -0400

    init files and queue class

---
+++ Stacks-And-Queues/Queue.py
@@ -0,0 +1,14 @@
+
+
+class Queue(list):
+	'''
+	Data structure definition 
+	'''
+	def __init__(self, queue=[]):
+		self.queue = queue
+	
+	def enqueue(element):
+		self.queue.append(element)
+
+	def dequeue():
+		self.queue.pop(0)
