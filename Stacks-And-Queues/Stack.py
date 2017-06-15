de50097d1179d4f7bf5b5cbdd11dd145e05efd5d
Adolfo Reyes <adolfo0620@gmail.com>
2017-06-15 15:48:20 -0400

    adding assertions for stack

--- Stacks-And-Queues/Stack.py
+++ Stacks-And-Queues/Stack.py
@@ -6,17 +6,36 @@ class Stack(list):
 	
 	def push(self, item):
 		self.insert(0, item)
+		return self
 	
 	def pop(self):
 		if self:
-			self.pop(0)
+			return super(Stack, self).pop(0)
+			
 
 class TestStack(unittest.TestCase):
 	def test_stack(self):
-		pass
-	
+		stack = Stack()
+		self.assertEqual(stack, stack)
+		
+		stack_items = ['a', 'b', 'c']
+		stack = Stack(stack_items)
+		self.assertEqual(stack, stack_items)
+
 	def test_push(self):
-		pass
+		stack = Stack()
+		stack.push('a')
+		self.assertEqual(stack, ['a'])
+		stack.push('b')
+		self.assertEqual(stack, ['b', 'a'])
 	
 	def test_pop(self):
-		pass
+		stack = Stack(['a', 'b', 'c'])
+		self.assertEqual(stack.pop(), 'a')
+		self.assertEqual(stack.pop(), 'b')
+		self.assertEqual(stack.pop(), 'c')
+		self.assertEqual(stack.pop(), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
