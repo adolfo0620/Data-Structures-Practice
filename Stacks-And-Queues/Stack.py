23d8c2e66b9742acb753a11012ddfbe52d6a9370
Adolfo Reyes <adolfo0620@gmail.com>
2017-06-15 15:34:06 -0400

    adding empty testcase

--- Stacks-And-Queues/Stack.py
+++ Stacks-And-Queues/Stack.py
@@ -1,7 +1,22 @@
+import unittest
+
 class Stack(list):
 	def __init__(self, stack=[]):
 		self.extend(stack)
-	def push(self):
-		pass
+	
+	def push(self, item):
+		self.insert(0, item)
+	
 	def pop(self):
+		if self:
+			self.pop(0)
+
+class TestStack(unittest.TestCase):
+	def test_stack(self):
+		pass
+	
+	def test_push(self):
+		pass
+	
+	def test_pop(self):
 		pass
